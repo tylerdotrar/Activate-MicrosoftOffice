3411b301585347c5f9debd8ddd3b13186376d529
unknown <kushslayer69420@usa.com>
2023-09-27 12:16:26 -0400

    Version 2.0.0: Optional version rollback & disable auto-updates.

--- Activate-MicrosoftOffice.ps1
+++ Activate-MicrosoftOffice.ps1
@@ -1,28 +1,39 @@
 ﻿function Activate-MicrosoftOffice {
 #.SYNOPSIS
 # Activate Microsoft Office Professional Plus 2016 - 2021
-# ARBITRARY VERSION NUMBER:  1.1.1
+# ARBITRARY VERSION NUMBER:  2.0.0
 # AUTHOR:  Tyler McCann (@tylerdotrar)
 #
 #.DESCRIPTION
 # Automatically detect and activate a locally installed Microsoft Office instance with a Professional Plus
-# KMS client key using either a publically available KMS server (default) or a specfied KMS server.  This
+# KMS client key, using either a publicly available KMS server (default) or a specfied KMS server.  This
 # script supports both 32-bit and 64-bit versions of Microsoft Office 2016, 2019, and 2021.
-# Note: This script must run elevated.
+#
+# As of version 2.0.0, this script will rollback the Microsoft Office version to '16.0.13801.20266' and 
+# set a registry key to disable automatic updates.  This should prevent the annoying 'GET GENUINE OFFICE' 
+# banner that plagued earlier versions of this script when using these KMS client keys.
+# 
+# Usage Notes:
+# o This script must run elevated (i.e., as Administrator)
+# o This script contains a list of known public KMS servers and will automatically attempt
+#   to use them if the user doesn't specify a specific KMS server and/or port.
+# o If you don't want to disable automatic updates or rollback your Office version, run
+#   this script with the '-DontRollBack' parameter.
 #
 # Parameters:
-#   -KMSserver    -->  Domain/IP of specified KMS server
-#   -KMSport      -->  Port of specified KMS server
+#   -KMSserver     -->  Target domain/IP of a specified KMS server
+#   -KMSport       -->  Target port of a specified KMS server
+#   -DontRollBack  -->  Don't rollback Office version and leave automatic updates enabled
 #
 # Official Microsoft Office Downloads:
-#   + Microsoft Office Professional Plus 2021:
-#   - Download URL : https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/ProPlus2021Retail.img
+#   [+] Microsoft Office Professional Plus 2021:
+#    o  Download Link: https://officecdn.microsoft.com/db/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/ProPlus2021Retail.img
 #
-#   + Microsoft Office Professional Plus 2019:
-#   - Download URL : https://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/ProPlus2019Retail.img
+#   [+] Microsoft Office Professional Plus 2019:
+#    o  Download Link: https://officecdn.microsoft.com/pr/492350f6-3a01-4f97-b9c0-c7c6ddf67d60/media/en-us/ProPlus2019Retail.img
 #
-#   + Microsoft Office Professional Plus 2016:
-#   - Download URL : <N/A>
+#   [+] Microsoft Office Professional Plus 2016:
+#    o  Download Link: <N/A>
 #
 #.LINK
 # https://github.com/tylerdotrar/Activate-MicrosoftOffice
@@ -31,58 +42,68 @@
     Param ( 
         [string]$KMSserver,
         [int]   $KMSport,
+        [switch]$DontRollBack,
         [switch]$Help
     )
 
 
-    # Retrun Help Information
+    # Return Get-Help Information
     if ($Help) { return (Get-Help Activate-MicrosoftOffice) }
+
 
     # Determine if user has elevated privileges
     $User    = [Security.Principal.WindowsIdentity]::GetCurrent();
     $isAdmin = (New-Object Security.Principal.WindowsPrincipal $User).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-    if (!$isAdmin) { return (Write-Host 'This function requires elevated privileges.' -ForegroundColor Red) }
+    if (!$isAdmin) { return (Write-Host '[-] This function requires elevated privileges.' -ForegroundColor Red) }
     
 
-    ## Step 1
-    Write-Host 'Determining if Office 64-bit or 32-bit version is installed...' -ForegroundColor Yellow
+    ## Step 1: Verify Office is installed
+    Write-Host '[+] Determining if Office 64-bit or 32-bit version is installed...' -ForegroundColor Yellow
     $PreActivate  = "$PWD"
     $OfficeDir    = "$env:ProgramFiles\Microsoft Office\Office16"
     $OfficeDirx86 = "${env:ProgramFiles(x86)}\Microsoft Office\Office16"
 
-    if (Test-Path -LiteralPath $OfficeDir)        { cd -LiteralPath $OfficeDir    ; Write-Host "- 64-bit`n" }
-    elseif (Test-Path -LiteralPath $OfficeDirx86) { cd -LiteralPath $OfficeDirx86 ; Write-host "- 32-bit`n" }
-    else { return (Write-Host "- Failed to find Microsoft Office.`n" -ForegroundColor Red) }
+
+    if (Test-Path -LiteralPath $OfficeDir)        { cd -LiteralPath $OfficeDir    ; Write-Host " o  64-bit`n" }
+    elseif (Test-Path -LiteralPath $OfficeDirx86) { cd -LiteralPath $OfficeDirx86 ; Write-host " o  32-bit`n" }
+    else { return (Write-Host "[-] Failed to find Microsoft Office.`n" -ForegroundColor Red) }
 
     
-    ## Step 2
-    Write-Host 'Determining version of Office to activate...' -ForegroundColor Yellow
+    ## Step 2: Determine version of Office installed
+    Write-Host "[+] Determining version of Office to activate..." -ForegroundColor Yellow
     $OfficeStatus  = cscript /nologo ospp.vbs /dstatus
     $OfficeVersion = ($OfficeStatus | Select-String -Pattern "ProPlus(\d{4})?").Matches.Value
     $CurrentKey    = (($OfficeStatus | Select-String -Pattern "product key:") -as [string]).Split(' ')[-1]
 
+
     if (!$OfficeVersion) {
         cd -LiteralPath $PreActivate
-        return (Write-Host "- Initial Office license not detected; please first-time open a Microsoft Office executable or reboot your computer.`n" -ForegroundColor Red)
+        return (Write-Host "[-] Initial Office license not detected.  Please first-time open a Microsoft Office executable or reboot your computer.`n" -ForegroundColor Red)
     }
     
+
     # Microsoft Office KMS Client Key Table
     $KeyTable = @(
-        [PSCustomObject]@{OfficeName="Microsoft Office 2021";OfficeVersion="ProPlus2021";Key="FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH"},
-        [PSCustomObject]@{OfficeName="Microsoft Office 2019";OfficeVersion="ProPlus2019";Key="NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP"},
-        [PSCustomObject]@{OfficeName="Microsoft Office 2016";OfficeVersion="ProPlus"    ;Key="XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99"}
+        [PSCustomObject]@{OfficeName="Microsoft Office 2021"; OfficeVersion="ProPlus2021"; Key="FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH"},
+        [PSCustomObject]@{OfficeName="Microsoft Office 2019"; OfficeVersion="ProPlus2019"; Key="NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP"},
+        [PSCustomObject]@{OfficeName="Microsoft Office 2016"; OfficeVersion="ProPlus"    ; Key="XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99"}
     )
 
+
     $KeyTable | % { if ($OfficeVersion -eq $_.OfficeVersion) { $ActivationKey = $_.Key ; $OfficeName = $_.OfficeName } }
-    Write-Host "- $OfficeName`n"
+    Write-Host " o  $OfficeName`n"
 
 
-    ## Step 3
-    if ($KMSserver) { Write-Host 'Testing connection to input KMS server for activation...'     -ForegroundColor Yellow }
-    else            { Write-Host 'Determining which public KMS server to use for activation...' -ForegroundColor Yellow }
+    ## Step 3: Validate KMS server connectivity
+    if ($KMSserver) { Write-Host "[+] Testing connection to input KMS server for activation..." -ForegroundColor Yellow     }
+    else            { Write-Host "[+] Determining which public KMS server to use for activation..." -ForegroundColor Yellow }
+
 
     # User input KMS server
     if ($KMSserver) { $ServerList = @("$KMSserver") }
+
+
+    # Known public KMS servers
     else {
         $ServerList += @(
             'e8.us.to',
@@ -92,38 +113,105 @@
         )
     }
 
+
+    # Test KMS server connection(s)
     foreach ($Server in $ServerList) {
-        # User input KMS port
+        # Default KMS Port: 1688
         if (!$KMSport) { $KMsport = 1688 }
         $Connection = Test-NetConnection -ComputerName $Server -Port $KMSport
 
         if ($Connection.TcpTestSucceeded) { $KMS_Server = $Server ; break }
     }
 
+
+    # Failed to connect to KMS server(s)
     if (!$KMS_Server) {
         cd -LiteralPath $PreActivate
-        return (Write-Host "- Failed to connect to any KMS servers; please try a different server.`n(Use 'Activate-MicrosoftOffice' with params '-KMSserver' & '-KMSport').`n" -ForegroundColor Red)
+        return (Write-Host "[-] Failed to connect to any KMS servers.  Try using the '-KMSserver' parameter to specify a different server.`n" -ForegroundColor Red)
     }
-    Write-Host "- ${KMS_Server}`n"
+    Write-Host " o  ${KMS_Server}`n"
 
 
-    ## Step 4
-    Write-Host 'Converting retail license to volume license...' -ForegroundColor Yellow
+    ## Step 4: Change default license to volume license (aka able to activate with KMS keys)
+    Write-Host "[+] Converting retail license(s) to volume license(s)..." -ForegroundColor Yellow
     $Licenses = (Get-ChildItem "..\root\Licenses16\${OfficeVersion}VL_KMS*.xrm-ms").FullName 2>$NULL
 
-    if ($Licenses -eq $NULL) { Write-Host "- Skipping`n" }
+
+    if ($Licenses -eq $NULL) { Write-Host " o  Skipping" }
     else { $Licenses | % { cscript /nologo ospp.vbs /inslic:$_ } }
 
 
-    ## Step 5
-    Write-Host 'Activating Microsoft Office using KMS client key...' -ForegroundColor Yellow
-    cscript /nologo ospp.vbs /sethst:${KMS_Server}
-    cscript /nologo ospp.vbs /setprt:$KMSport
-    cscript /nologo ospp.vbs /unpkey:$CurrentKey
-    cscript /nologo ospp.vbs /inpkey:$ActivationKey
-    cscript /nologo ospp.vbs /act
+    ## Step 5: Activate Office with KMS key
+    Write-Host "`n[+] Activating Microsoft Office using KMS client key..." -ForegroundColor Yellow
+
+    Write-Host " o  Setting KMS Hostname : '${KMS_Server}'"
+    cscript /nologo ospp.vbs /sethst:${KMS_Server}  ; Start-Sleep -Seconds 3
+
+    Write-Host " o  Setting KMS Port     : $KMSport"
+    cscript /nologo ospp.vbs /setprt:$KMSport       ; Start-Sleep -Seconds 3
+
+    Write-Host " o  Uninstalling current product key..."
+    cscript /nologo ospp.vbs /unpkey:$CurrentKey    ; Start-Sleep -Seconds 3
+
+    Write-Host " o  Installing KMS product key..."
+    cscript /nologo ospp.vbs /inpkey:$ActivationKey ; Start-Sleep -Seconds 3
+
+    Write-Host " o  Activating new product key..."
+    cscript /nologo ospp.vbs /act                   ; Start-Sleep -Seconds 3
+
+
+    ### Version 2.0.0 ###
+    if (!$DontRollBack) {
+
+        # Step 6: Rollback Office version to a bannerless version
+        Write-Host "`n[+] Rolling back Office version to remove 'GET GENUINE OFFICE' banner..." -ForegroundColor Yellow
+
+        $ClickToRun = “$env:ProgramFiles\Common Files\microsoft shared\ClickToRun”
+        $Continue   = $TRUE
+
+        # Validate directory exists
+        if (!(Test-Path -LiteralPath $ClickToRun)) {
+            Write-Host "[-] Unable to find the '$ClickToRun' directory. Aborting."
+            $Continue = $FALSE
+        }
+
+        if ($Continue) {
+            
+            # Rollback method doesn't work on Office 2021
+            if ($OfficeVersion -eq 'ProPlus2021') {
+                Write-Host " o  Version rollback only works on Office 2016 - 2019.`n"
+            }
+
+            # Use OfficeC2RClient to rollback version
+            else {
+                Write-Host " o  Rolling back to version '16.0.13801.20266'"
+                Write-host " o  This might take a while..."
+
+                cd $ClickToRun
+                .\OfficeC2RClient.exe /update user updatetoversion=16.0.13801.20266
+                
+                # Wait for OfficeC2Client to execute and close
+                Start-Sleep -Seconds 10
+                while (Get-Process -Name OfficeC2RClient 2>$NULL) { $SuccessfullyExecuted = $TRUE }
+                if (!$SuccessfullyExecuted) {  Write-Host "[-]  Failed to launch 'OfficeC2RClient' executable." }
+            }
+            
+
+            # Step 7: Disable automatic updates
+            Write-Host "`n[+] Disabling Microsoft Office automatic updates..." -ForegroundColor Yellow
+            $DisableOfficeUpdates = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate'
+
+            if (!(Test-Path $DisableOfficeUpdates)) { New-Item $DisableOfficeUpdates -Force | Out-Null }
+            Set-ItemProperty -Path $DisableOfficeUpdates -Name 'enableautomaticupdates' -Value 0
+
+            Write-Host "  o  Location    : 'HKLM\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate'"
+            Write-Host "  o  Registy Key : 'enableautomaticupdates' --> 0"
+        }
+    }
+    else { Write-Host "`n[+] Skipping version rollback." -ForegroundColor Yellow }
+
 
     # Return to original directory post-activation
     cd -LiteralPath $PreActivate
-    return (Write-Host 'Complete.' -ForegroundColor Green)
+    return (Write-Host "`n[+] Complete." -ForegroundColor Yellow)
 }
